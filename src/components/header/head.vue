216879b798b79be1847317679875db498790f871
yadong <yangyadong1900@gmail.com>
2021-04-14 21:48:26 +0800

    init

---
+++ src/components/header/head.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <span class="icon"> </span>
+    <h1 class="text">FW Music</h1>
+    <router-link></router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+    
+<style lang="scss" scoped>
+.header {
+  height: 44px;
+  text-align: center;
+  color: $color-theme;
+  font-size: 0;
+  .icon {
+    display: inline-block;
+    width: 30px;
+    height: 32px;
+    vertical-align: top;
+    margin-top: 6px;
+    margin-right: 9px;
+    @include bg-image("logo");
+    background-size: 30px 32px;
+  }
+  .text {
+    display: inline-block;
+    vertical-align: top;
+    line-height: 44px;
+    font-size: $font-size-large;
+  }
+}
+</style>
