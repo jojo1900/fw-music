b2a6e71f2547cfa347477ead290fd219e8ab7c66
Robin <yang969397449@qq.com>
2021-06-16 01:04:16 +0800

    m

--- src/components/base/loading/directive.js
+++ src/components/base/loading/directive.js
@@ -1,12 +1,27 @@
-import { createApp } from 'vue';
+import {
+  createApp
+} from 'vue';
 import Loading from './loading'
 const loadingDirective = {
-    mounted(el, binding) {
-        const app = createApp(Loading)
-        
-    },
-    update(el, binding) {
-
+  mounted(el, binding) {
+    const app = createApp(Loading)
+    const instance = app.mount(document.createElement('div'))
+    //把实例保存下来
+    el.instance = instance
+  },
+  update(el, binding) {
+    if (el.value !== el.oldValue) {
+      el.value ? append(el) : remove(el)
     }
+  }
 
+
+}
+
+function append(el) {
+  el.appendChild(el.instance.$el)
+}
+
+function remove(el) {
+  el.removeChild(el.instance.$el)
 }
