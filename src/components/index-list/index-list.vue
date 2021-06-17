bc3a38ae2b4c3f0092cde32f4416e1462bbdb48e
yadong <yangyadong1900@gmail.com>
2021-06-18 01:25:22 +0800

    singer

---
+++ src/components/index-list/index-list.vue
@@ -0,0 +1,72 @@
+<template>
+  <Scroll class="index-list">
+    <ul>
+      <li v-for="group in data" :key="group.title" class="group">
+        <h2 class="title">{{ group.title }}</h2>
+        <ul>
+          <li v-for="singer in group.list" :key="singer.id" class="singer">
+            <img class="avatar" v-lazy="singer.pic" />
+            <span class="name">{{ singer.name }}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </Scroll>
+</template>
+
+<script>
+import Scroll from "@/components/base/scroll/scroll";
+export default {
+  components: {
+    Scroll
+  },
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.index-list {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: $color-background;
+
+  .group {
+    padding-bottom: 30px;
+    .title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
+    }
+    .singer {
+      display: flex;
+      align-items: center;
+      padding: 20px 0 0 30px;
+      .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      }
+      .name {
+        margin-left: 20px;
+        color: $color-text-l;
+        font-size: $font-size-medium;
+      }
+    }
+  }
+}
+</style>
--- src/views/singer.vue
+++ src/views/singer.vue
@@ -2,6 +2,18 @@
   <div class="singer">111</div>
 </template>
 
+<script>
+import { getSingerList } from "../service/singer";
+export default {
+  name: "singer",
+
+  async created() {
+    const result = await getSingerList();
+    console.log(result);
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 .singer {
 }
