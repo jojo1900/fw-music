f4ba86e77309bf93e25a42296d9cc48752c842f8
yadong <yangyadong1900@gmail.com>
2021-06-18 01:49:42 +0800

    m

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="singer">111</div>
+  <div class="singer">
+    <IndexList :data="data"></IndexList>
+  </div>
 </template>
 
 <script>
 import { getSingerList } from "../service/singer";
+import IndexList from "../components/index-list/index-list.vue";
 export default {
   name: "singer",
-
+  components: {
+    IndexList
+  },
+  data() {
+    return {
+      data: []
+    };
+  },
   async created() {
     const result = await getSingerList();
+    this.data = result;
     console.log(result);
   }
 };
